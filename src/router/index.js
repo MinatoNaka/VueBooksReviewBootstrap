8771bd16970293565cf145a3c2dd7f1f2920654f
aq_minato.naka <minato.naka@asia-quest.jp>
2020-10-02 08:21:46 +0900

    ルーティング定義

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,29 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import BookSearch from "../views/BookSearch.vue";
+import BookForm from "../views/BookForm.vue";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: BookSearch,
+  },
+  {
+    path: "/form",
+    name: "form",
+    component: BookForm,
+  },
+  { path: "*", redirect: "/" },
+];
 
 const router = new VueRouter({
   mode: "history",
