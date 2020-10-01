4ae2b99997a24041eb91498978ff03c58e64a917
aq_minato.naka <minato.naka@asia-quest.jp>
2020-10-02 08:16:29 +0900

    既存ファイルをフォーマット

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import './assets/scss/custom.scss'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "./assets/scss/custom.scss";
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
