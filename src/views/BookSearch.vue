04b1e6980f11a35c59f74691af512d841fc9ede9
aq_minato.naka <minato.naka@asia-quest.jp>
2020-10-07 08:05:25 +0900

    本検索機能実装

--- src/views/BookSearch.vue
+++ src/views/BookSearch.vue
@@ -1,11 +1,64 @@
 <template>
-  <div>
-    book-search
+  <div id="search">
+    <b-form inline class="mt-3">
+      <b-input-group size="sm" class="mb-2">
+        <b-input-group-prepend>
+          <b-button variant="c-highlight"
+            ><b-icon icon="search" @click="onclick"></b-icon
+          ></b-button>
+        </b-input-group-prepend>
+        <b-form-input
+          type="search"
+          placeholder="Search terms"
+          v-model="keyword"
+        ></b-form-input>
+      </b-input-group>
+    </b-form>
+    <hr />
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        {{ book.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "book-search",
+  components: {},
+  data() {
+    return {
+      keyword: "vuejs",
+      books: [],
+    };
+  },
+  methods: {
+    onclick: function() {
+      this.$http(
+        "https://www.googleapis.com/books/v1/volumes?q=" + this.keyword
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.books = [];
+          for (let b of data.items) {
+            let authors = b.volumeInfo.authors;
+            let price = b.saleInfo.listPrice;
+            let img = b.volumeInfo.imageLinks;
+            this.books.push({
+              id: b.id,
+              title: b.volumeInfo.title,
+              author: authors ? authors.join(",") : "",
+              price: price ? price.amount : "-",
+              publisher: b.volumeInfo.publisher,
+              published: b.volumeInfo.publishedDate,
+              image: img ? img.smallThumbnail : "",
+            });
+          }
+        });
+    },
+  },
 };
 </script>
